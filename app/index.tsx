48f3d28467e349bdf4863be2092e8dd91ac95ab1
anandkushwaha <ait.17bcs388@gmail.com>
2024-12-07 03:07:28 +0530

    update

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,15 @@
+import { Link } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
 
 export default function Index() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text className="text-3xl font-pblack">Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href="/home" className="color-blue-200">
+        Home
+      </Link>
     </View>
   );
 }
